77008591eb12bd7844cf55dc7a9c0840352ed2ac
yarul12GC <yahir634gallo@gmail.com>
2024-07-25 17:10:36 -0600

    se agregan bloques

---
+++ admin/bloque19/tablasb19.php
@@ -0,0 +1,27 @@
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>SECCION.</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td rowspan="15" class="text-center">1</td>
+        </tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+
+
+    </tbody>
+</table>
